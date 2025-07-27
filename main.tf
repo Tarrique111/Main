c19ba14477408af1473d0ca6d13b6a229e2166cf
Tarrique <tarriques@aol.com>
2025-07-27 21:19:11 +0530

    [learing-007] This is first commit

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"  # Update if you're targeting a different region
+  region = "us-east-1"  # Change to your desired AWS region
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0cbbe2c6a1bb2ad63"  # Amazon Linux 2 AMI (us-east-1)
-  instance_type = "t3.micro"
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (for us-east-1)
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Terraform-Example"
